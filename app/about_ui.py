2139248e6d69eecc33438a205ab0eccebb507d54
krknwtn <krn@outlook.com>
2025-09-05 13:35:49 +0200

    link to eth research collection

--- app/about_ui.py
+++ app/about_ui.py
@@ -22,9 +22,9 @@ def about_page():
         st.caption("Published in a CISBAT 2025 special issue of IOP's JOURNAL OF PHYSICS Conference Series")
         st.markdown("Developing building archetypes from urban data is essential for urban building energy modeling (UBEM), yet current approaches lack generalizability, particularly regarding categorical data. This paper compares k-means, k-medoids, and k-prototypes clustering using Zurich’s building registry to evaluate the impact of preprocessing and algorithm choice. Results show preprocessing had limited effect on clustering, while k-prototypes—capable of handling numerical and categorical data concurrently—produced practical, interpretable clusters. The method’s simplicity and robust handling of mixed-type data suggest k-prototypes can streamline UBEM archetype generation workflows.")
         st.link_button(
-            "Paper",
+            "ETH Research Collection",
             icon=":material/north_east:",
-            url="https://cisbat.epfl.ch/"
+            url="https://www.research-collection.ethz.ch/items/78c3ddb8-cd60-4e33-87ad-e6091ec4dda9"
         )
 
 about_page()
